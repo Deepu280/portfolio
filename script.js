f0722dfdb00bd43af7e567a1fb7f048b436cd4fa
Your Name <artdeep80@gmail.com>
2023-11-06 00:37:20 +0530

    q

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab")
 }
+const cursorDot = document.querySelector("data-cursor-dot");
+const cursorOutline = document.querySelector("data-cursor-outline")
+
+window.addEventListener("mousemove", function (e){
+
+const posX = e.clientX;
+const posY = e.clientY;
+
+cursorDot.style.left = `${posX}px`;
+cursorDot.style.top = `${posY}px`;
+
+//cursorOutline.style.left = `${posX}px`;
+//cursorOutline.style.top = `${posY}px`;
+
+});
